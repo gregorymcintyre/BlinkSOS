01ac0e25993d0e6fa7c9f6e73888c8a3656ebc05
Greg McIntyre <gregmcintyre1984@gmail.com>
2018-07-07 21:21:36 +1000

    Clean up comments

--- BlinkSOS.ino
+++ BlinkSOS.ino
@@ -1,5 +1,5 @@
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT); //intialise the built in LED
+  pinMode(LED_BUILTIN, OUTPUT);       //Intialise the built in LED
 }
 
 void loop() {
@@ -8,28 +8,28 @@ void loop() {
     signal('o');
     signal('s');
     
-    delay(2000);                      //cosmetic Delay
+    delay(2000);                      //Cosmetic Delay
 }
 
   void signal(char c){
     switch (c){
       case 's':
           for (int i = 0 ; i < 3 ; i++){
-            digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-           delay(250);                        // wait for half a second
-            digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-            delay(500);                       // wait for a second
+            digitalWrite(LED_BUILTIN, HIGH);    // turn the LED on (HIGH is the voltage level)
+            delay(250);                         // wait for 1/4 a second
+            digitalWrite(LED_BUILTIN, LOW);     // turn the LED off by making the voltage LOW
+            delay(500);                         // wait for 1/2 second
           }
-          delay(1000);                        //Character Pause
+          delay(1000);                          //Character Pause
       break;
       case 'o':
         for (int i = 0 ; i < 3 ; i++){
-        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(250);                        // wait for half a second
-        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-        delay(1500);                       // wait for a second
+          digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+          delay(250);                        // wait for 1/4 second
+          digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+          delay(1500);                       // wait for a 3/2 second
         }
-        delay(1000);                      //Character Pause
+        delay(1000);                        //Character Pause
       break;
      }
   } 
